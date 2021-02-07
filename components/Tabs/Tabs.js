17de7d576bfa5643afd8dcf05534c9544fd9d606
Chen zeng <1476104805@qq.com>
2021-02-07 20:42:23 +0800

    components

---
+++ components/Tabs/Tabs.js
@@ -0,0 +1,40 @@
+// components/Tabs/Tabs.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    aaa:{
+      type:String,
+      value:""
+    },
+    tabs:{
+      type:Array,
+      value:[]
+    }
+  },
+
+
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleItemTap(e){
+      console.log(e);
+      const tabs=this.data.tabs;
+      const data=e.currentTarget.dataset.index;
+      for(let i=0,lenI=tabs.length;i<lenI;++i)
+        tabs[i].isActive=false;
+      for(let j=0,lenJ=tabs.length;j<lenJ;++j){
+        if(tabs[j].id==data){
+           tabs[j].isActive=true;
+           break;
+        }
+      }
+      this.setData({
+        tabs
+      })
+    }
+  }
+})
---
+++ pages/demo07/demo07.js
@@ -0,0 +1,32 @@
+// pages/demo07/demo07.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs: [{
+        id: 0,
+        name: "首页",
+        isActive: true
+      },
+      {
+        id: 1,
+        name: "原创",
+        isActive: false
+      },
+      {
+        id: 2,
+        name: "分类",
+        isActive: false
+      },
+      {
+        id: 3,
+        name: "关于",
+        isActive: false
+      },
+    ],
+  },
+
+
+})
